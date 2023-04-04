b769e6461803a9792823766e974f06b2c168d08b
ynakayu <9111754+ynakayu@users.noreply.github.com>
2023-04-04 12:47:26 +0900

    10-05-add-task

--- app.vue
+++ app.vue
@@ -1,16 +1,31 @@
 <template>
   <div>
     <h1>To Do</h1>
-    <form>
-      <input />
+    <form @submit.prevent="addTask">
+      <input v-model="newTask" name="newTask" autocomplete="off" />
       <button>Add</button>
     </form>
     <ul>
-      <li>
-        <span>ToDoテスト</span>
+      <li v-for="(task) in tasks" :key="task">
+        <span>{{ task }}</span>
         <button>Delete</button>
       </li>
     </ul>
     <button>Clear</button>
   </div>
 </template>
+<script setup>
+  const tasks = useCookie(
+    'tasks',
+    {
+      default: () => []
+    }
+  )
+  const newTask = ref('')
+  function addTask() {
+    if(newTask.value.length >= 1){
+      tasks.value.push(newTask.value)
+    }
+    newTask.value = ''
+  }
+</script>
