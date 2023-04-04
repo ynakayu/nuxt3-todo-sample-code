ccf6604246aef6df6e0d0aad5167c2af53386e5a
ynakayu <9111754+ynakayu@users.noreply.github.com>
2023-04-04 12:58:48 +0900

    10-06-delete-and-clear

--- app.vue
+++ app.vue
@@ -6,12 +6,12 @@
       <button>Add</button>
     </form>
     <ul>
-      <li v-for="(task) in tasks" :key="task">
+      <li v-for="(task, index) in tasks" :key="task">
         <span>{{ task }}</span>
-        <button>Delete</button>
+        <button @click="deleteTask(index)">Delete</button>
       </li>
     </ul>
-    <button>Clear</button>
+    <button @click="clearTask">Clear</button>
   </div>
 </template>
 <script setup>
@@ -28,4 +28,13 @@
     }
     newTask.value = ''
   }
+  
+  function deleteTask(index) {
+    tasks.value.splice(index, 1)
+  }
+
+  function clearTask() {
+    tasks.value = []
+  }
+
 </script>
